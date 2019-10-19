dce7c2133c32403c45c4ba289a30d5d42a07f04c
Amir Arbabian <gretam911@gmail.com>
2019-10-19 19:59:54 +0300

    Implement spread paint square function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,67 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { PointCoordinates } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   readonly Array = Array;
-  readonly gridSizeX = 200;
-  readonly gridSizeY = 110;
-  readonly speed = 10;
+  readonly gridSizeX = 125;
+  readonly gridSizeY = 60;
+  readonly gridRangeX = [...Array(this.gridSizeX).keys()];
+  readonly gridRangeY = [...Array(this.gridSizeY).keys()];
+  readonly speed = 100;
 
-  pointsCheckFlags: Array<Array<boolean>> = [
-    ...Array(this.gridSizeX).keys(),
-  ].map(() => [...Array(this.gridSizeY).keys()].map(() => false));
+  pointsValues: Array<Array<number>> = this.gridRangeX.map(() => this.gridRangeY.map(() => 0));
 
   trackByFn(index, item) {
     return index;
   }
 
-  onPointClicked({ x, y }: PointCoordinates) {
-    let i = 0;
-    let j = 0;
-    const timer = setInterval(() => {
-      if (i < this.gridSizeX) { this.pointsCheckFlags[i][y] = true; }
-      if (j < this.gridSizeY) { this.pointsCheckFlags[x][j] = true; }
-      i++; j++;
-      if (i >= this.gridSizeX && j >= this.gridSizeY) { clearInterval(timer); }
-    }, this.speed);
+  async spreadPaintLines({ x, y }: PointCoordinates) {
+    this.pointsValues[x][y]++;
+    for (let i = 1; i < Math.max(this.gridSizeX, this.gridSizeY); i++) {
+      const topY = y - i;
+      const botY = y + i;
+      const leftX = x - i;
+      const rightX = x + i;
+
+      if (leftX >= 0) { this.pointsValues[leftX][y]++; }
+      if (rightX < this.gridSizeX) { this.pointsValues[rightX][y]++; }
+
+      if (topY >= 0) { this.pointsValues[x][topY]++; }
+      if (botY < this.gridSizeY) { this.pointsValues[x][botY]++; }
+
+      await this.skipTime();
+    }
+  }
+
+  async spreadPaintSquare({ x, y }: PointCoordinates) {
+    this.pointsValues[x][y]++;
+    for (let i = 1; i < Math.max(this.gridSizeX, this.gridSizeY) ** 2; i++) {
+      for (let j = 0; j <= i; j++) {
+        const topY = y - i + j;
+        const botY = y + i - j;
+        const rightX = x + j;
+        const leftX = x - j;
+
+        if (topY >= 0) {
+          if (rightX < this.gridSizeX) { this.pointsValues[rightX][topY]++; }
+          if (leftX >= 0 && leftX !== rightX) { this.pointsValues[leftX][topY]++; }
+        }
+
+        if (botY < this.gridSizeY && botY !== topY) {
+          if (rightX < this.gridSizeX) { this.pointsValues[rightX][botY]++; }
+          if (leftX >= 0 && leftX !== rightX) { this.pointsValues[leftX][botY]++; }
+        }
+      }
+      await this.skipTime();
+    }
+  }
+
+  private skipTime() {
+    return new Promise(resolve => setTimeout(resolve, this.speed));
   }
 }
