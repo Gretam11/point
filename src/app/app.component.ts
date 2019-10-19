4f3ea868db2c36b42ee6e486ac10778ec7c3c505
Amir Arbabian <gretam911@gmail.com>
2019-10-19 17:06:05 +0300

    Add grid to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'point';
+  readonly Array = Array;
+  readonly gridSizeX = 125;
+  readonly gridSizeY = 60;
+
+  pointsCheckFlags: Array<Array<boolean>> = [
+    ...Array(this.gridSizeX).keys(),
+  ].map(() => [...Array(this.gridSizeY).keys()].map(() => false));
+
+  trackByFn(index, item) {
+    return index;
+  }
 }
